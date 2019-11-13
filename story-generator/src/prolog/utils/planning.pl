10f281415089ef63a0a5a110d501cb96c550c0c5
Rafael Rubim <rafael.rubim.cabral@gmail.com>
2019-11-13 03:25:49 -0300

    Improve planner

--- story-generator/src/prolog/utils/planning.pl
+++ story-generator/src/prolog/utils/planning.pl
@@ -1,14 +1,27 @@
 :- use_module(set).
 
-facts([
-  pos(a),
-  pos(b),
-  pos(c),
-  onPos(monkey, a),
-  onPos(stairs, b),
-  onPos(banana, c),
-  onGround(monkey)
-  ]).
+% PLANNER IMPLEMENTATION
+
+% satisfiedGoalsList(goalsList)
+satisfiedGoalsList(GoalsList) :-
+  maplist(call, GoalsList).
+
+% executeSideEffects(sideEffectsList)
+executeSideEffects(SideEffectsList) :-
+  maplist(ignore, SideEffectsList).
+
+negation(X, not(X)).
+negation(X, \+ X).
+
+isNegation(X) :-
+  negation(_, X), !. % green cut
+
+emptyIntersection(SetA, SetB) :-
+  setDiff(SetA, SetB, SetA).
+
+separateConditions(Conditions, Inclusions, Exclusions) :-
+  partition(isNegation, Conditions, Negations, Inclusions),
+  maplist(negation, Exclusions, Negations).
 
 % updatedFacts(facts, removedFacts, addedFacts, NewFacts)
 updatedFacts(Facts, RemovedFacts, AddedFacts, NewFacts) :-
@@ -17,13 +30,18 @@ updatedFacts(Facts, RemovedFacts, AddedFacts, NewFacts) :-
 
 % allowedActionExecution(Action, facts, NewFacts)
 allowedActionExecution(Action, Facts, NewFacts) :-
-  actionSpecification(Action, Conditions, RemovedFacts, AddedFacts),
-  subsetOf(Conditions, Facts),
-  updatedFacts(Facts, RemovedFacts, AddedFacts, NewFacts).
+  actionSpecification(Action, TypeSpecifications, PrologConditions, Conditions, RemovedFacts, AddedFacts, PrologSideEffects),
+  satisfiedGoalsList(TypeSpecifications),
+  satisfiedGoalsList(PrologConditions),
+  separateConditions(Conditions, Inclusions, Exclusions),
+  subsetOf(Inclusions, Facts),
+  emptyIntersection(Exclusions, Facts),
+  updatedFacts(Facts, RemovedFacts, AddedFacts, NewFacts),
+  executeSideEffects(PrologSideEffects).
 
 % planExecutionWithForbiddenStates(facts, goals, Plan, forbiddenStates)
 planExecutionWithForbiddenStates(Facts, Goals, [], _) :-
-  subsetOf(Goals, Facts).
+  subsetOf(Goals, Facts), !. % red cut
 planExecutionWithForbiddenStates(Facts, Goals, [FirstAction|RestOfPlan], ForbiddenStates) :-
   allowedActionExecution(FirstAction, Facts, NewFacts),
   not(setIsOneOf(NewFacts, ForbiddenStates)),
@@ -31,15 +49,33 @@ planExecutionWithForbiddenStates(Facts, Goals, [FirstAction|RestOfPlan], Forbidd
 
 % planExecution(facts, goals, Plan)
 planExecution(Facts, Goals, Plan) :-
-  planExecutionWithForbiddenStates(Facts, Goals, Plan, []).
+  planExecutionWithForbiddenStates(Facts, Goals, Plan, [Facts]).
 
-% actionSolution(facts, goals, Action)
-actionSolution(Facts, Goals, Action) :-
-  allowedActionExecution(Action, Facts, NewFacts),
-  subsetOf(Goals, NewFacts).
+% PLANNER EXAMPLE
 
-% actionSpecification(action, prologConditions, factConditions, effectRemovedFacts, effectAddedFacts, prologSideEffects).
+% Type facts
+place(a).
+place(b).
+place(c).
 
-actionSpecification(move(Y), [], [onPos(monkey, X), pos(Y), X \== Y], [onPos(monkey, X)], [onPos(monkey, Y)], []).
-%actionSpecification(carry(Y), [], [onPos(monkey, X), onPos(stairs, X)], [onPos(monkey, X), onPos(stairs, X)], [onPos(monkey, Y), onPos(stairs, Y)], []).
-%actionSpecification(win, [], [onPos(monkey, X), onPos(banana, X), \+ onGround(mokey)], [], [win], []).
+character(monkey).
+
+object(stairs).
+object(banana).
+
+% State facts
+facts([
+  onPos(monkey, a),
+  onPos(stairs, b),
+  onPos(banana, c),
+  onGround(monkey)
+  ]).
+
+% actionSpecification(action, typeSpecifications, prologConditions, conditions, removedFacts, addedFacts, prologSideEffects).
+actionSpecification(move(X, Y), [place(X), place(Y)], [X \== Y], [onPos(monkey, X), onGround(monkey)], [onPos(monkey, X)], [onPos(monkey, Y)], []).
+actionSpecification(carry(X, Y), [place(X), place(Y)], [X \== Y], [onPos(monkey, X), onPos(stairs, X), onGround(monkey)], [onPos(monkey, X), onPos(stairs, X)], [onPos(monkey, Y), onPos(stairs, Y)], []).
+actionSpecification(climb(X), [place(X)], [], [onPos(monkey, X), onPos(stairs, X), onGround(monkey)], [onGround(monkey)], [], []).
+actionSpecification(win(X), [place(X)], [], [onPos(monkey, X), onPos(banana, X), \+ onGround(monkey)], [], [win], []).
+
+% The following query retrieves the plan:
+% facts(X), planExecution(X, [win], Plan).
