bab35a5fa4d74a21e553184757960ddb9b376e7d
Rafael Rubim <rafael.rubim.cabral@gmail.com>
2019-11-17 21:36:36 -0300

    Finish planAStar predicate with documentation

--- story-generator/src/prolog/utils/planning.pl
+++ story-generator/src/prolog/utils/planning.pl
@@ -4,7 +4,7 @@
  * <module> Planning
  * 
  *   This module implements a STRIPS-like planner with prolog functionalities. It consumes external predicates
- * for domain definition (actionSpec, type) and problem definition. It defines a predicate (plan) to
+ * for domain definition (actionSpec, type) and problem definition. It defines two predicates (plan, planAStar) to
  * return plans that satisfy the problem.
  * To use the module, an ontology similar to PDDL STRIPS must be followed to define a planning domain/problem.
  * It is explained below.
@@ -64,6 +64,7 @@
  * person(jorge).
  * 
  * location(hogwarts).
+ * location(araluen).
  * location(hyrule).
  * 
  * :- endProblemDefinition(domain_name).
@@ -78,8 +79,12 @@
  *   To use the planner, you call the "plan" predicate with the corresponding namespace. It also receives
  * a list of planner facts and planner goals (which are just like actionSpec's arg4). The planner will return you
  * a plan (list of actions in the order they should be executed) and the final set of facts, resulting from the
- * executing each action in the plan. A final example follows below, with the query and the result.
+ * executing each action in the plan. The predicate "planAStar" is almost the same, but it receives an Heuristic
+ * meta predicate that determines the cost of each action, returning also the PlanCost of the whole plan. "planAStar"
+ * Only returns the plan(s) with best score (lowest PlanCost). A final example follows below, with the queries and
+ * results to "plan" and "planAStar".
  * 
+ * ?- % This will return all plans that solve the problem.
  * ?- plan(
  * ?-   domain_name,                    % Namespace
  * ?-   [onLocation(jorge, hogwarts)],  % List of facts
@@ -87,8 +92,38 @@
  * ?-   Plan,                           % Returned plan
  * ?-   FinalFacts                      % Facts that remain after plan is executed
  * ?- ).
- * Plan = [move(jorge, hogwarts, hyrule)];
- * FinalFacts = [onLocation(jorge, hyrule)].
+ * Plan = [move(jorge,hogwarts,araluen),move(jorge,araluen,hyrule)],
+ * FinalFacts = [onLocation(jorge, hyrule)];
+ * Plan = [move(jorge,hogwarts,hyrule)],
+ * FinalFacts = [onLocation(jorge, hyrule)];
+ * false.
+ *
+ * ?- % Moving from hogwarts to hyrule directly costs 50. Any other action costs 5.
+ * ?- assert(
+ * ?-   heuristic(actionExecution(move(_,hogwarts, hyrule),_), 50) :- !
+ * ?- ).
+ * true.
+ *
+ * ?- assert(
+ * ?-   heuristic(_, 5)
+ * ?- ).
+ * true.
+ *
+ * ?- % This will only return the best plan that solves the problem, given the heuristic.
+ * ?- % It won't move directly from hogwarts to hyrule since it costs more.
+ * ?- planAStar(
+ * ?-   domain_name,                    % Namespace
+ * ?-   [onLocation(jorge, hogwarts)],  % List of facts
+ * ?-   [onLocation(jorge, hyrule)],    % List of goals
+ * ?-   heuristic,                      % Heuristic meta predicate
+ * ?-   Plan,                           % Returned plan
+ * ?-   PlanCost,                       % Total cost of the plan
+ * ?-   FinalFacts                      % Facts that remain after plan is executed
+ * ?- ).
+ * Plan = [move(jorge,hogwarts,araluen),move(jorge,araluen,hyrule)],
+ * PlanCost = 10,
+ * FinalFacts = [onLocation(jorge, hyrule)];
+ * false.
  * 
  * @author Rafael Rubim Cabral
  * @version 0.1.0
@@ -97,7 +132,7 @@
  * @see https://www.swi-prolog.org/pldoc/doc_for?object=section(%27packages/pldoc.html%27)
  **/
 
-% -------------------- External predicate interfaces
+% -------------------- External and meta predicate interfaces
 
 % actionSpec(-Action:compound_term, -TypeSpecs:list, -PrologConditions:list, -Conditions:list,
 %                     -RemovedFacts:list, -AddedFacts:list, -PrologSideEffects:list) is nondet
@@ -118,7 +153,25 @@
 %
 % True if Type is a user-defined type
 
-% -------------------- End of external predicate interfaces
+% 'typeFunctor'(-Object:atom) is nondet
+% 
+% @arg Object Object instantiated to the type
+%
+%   'typeFunctor' is not an actual atom, but could be any atom the user defines as a type.
+% True if Object is instantiated to the 'typeFunctor' type.
+
+
+% planAStar's "Heuristic" meta predicate:
+% 'heuristic'(++ActionExecution, -Cost) is det
+% 
+%   'heuristic' is not an actual atom, but could be any predicate name the user defines to use as
+% meta predicate. It receives an ActionExecution in the format
+% actionExecution(Action:compound_term, ObtainedFacts:list). Action is the action name defined by
+% an "actionSpec" predicate, with instantiated variables. ObtainedFacts is a set of facts the planner
+% will achieve after executing that action. The cost of this actionExecution should be returned through
+% Cost.
+
+% -------------------- End of external and meta predicate interfaces
 
 :- use_module(set).
 :- use_module(getRuntimeClauses).
@@ -236,6 +289,15 @@ deleteProblem(Namespace) :-
 plan(Namespace, Facts, Goals, Plan, FinalFacts) :-
   planWithForbiddenStates(Namespace, Facts, Goals, Plan, [Facts], FinalFacts).
 
+
+% planAStar(++Namespace:atom, ++Facts:list, ++Goals:list, :Heuristic:atom, -Plan:list, -PlanCost:number, -FinalFacts:list) is nondet
+% 
+% @arg Namespace This could be any atom. The Namespace used here should match the one used in "endDomainDefinition" and "endProblemDefinition".
+%
+%   True if Plan is a list of actions that make the planer Goals be satisfied from the initial Facts. The Heuristic meta predicate determines
+% the order in which actions happen to compute the Plan by associating costs to each action. That also determines the total PlanCost, which is
+% returned. Only the best plan is returned, unless that are other equivalent-costed ones. FinalFacts is the set of facts that remain after the
+% plan is executed.
 planAStar(_, Facts, Goals, _, [], 0, Facts) :-
   satisfiedPlannerGoals(Facts, Goals), !. % red cut
 planAStar(Namespace, Facts, Goals, Heuristic, Plan, PlanCost, FinalFacts) :-
@@ -245,71 +307,6 @@ planAStar(Namespace, Facts, Goals, Heuristic, Plan, PlanCost, FinalFacts) :-
   \+ empty_heap(NewHeap),
   planAStar_(Namespace, NewHeap, Goals, Heuristic, [Facts], ReversePlan, PlanCost, FinalFacts),
   reverse(ReversePlan, Plan).
-
-planAStar_(Namespace, Heap, Goals, Heuristic, ForbiddenStates, Plan, PlanCost, FinalFacts) :-
-  min_of_heap(Heap, HalfPlanCost, halfPlanExecution(HalfPlan, Facts)),
-  satisfiedPlannerGoals(Facts, Goals),
-  (
-    Plan = HalfPlan,
-    PlanCost = HalfPlanCost,
-    FinalFacts = Facts
-  ;
-    !, % red cut
-    get_from_heap(Heap, _, _, PopedHeap),
-    planAStar_(Namespace, PopedHeap, Goals, Heuristic, ForbiddenStates, Plan, PlanCost, FinalFacts)
-  ).
-planAStar_(Namespace, Heap, Goals, Heuristic, ForbiddenStates, Plan, PlanCost, FinalFacts) :-
-  openNonForbiddenActionsInHeap(Namespace, ForbiddenStates, Heuristic, Heap, NewHeap, OpenedFacts),
-  \+ empty_heap(NewHeap),
-  planAStar_(Namespace, NewHeap, Goals, Heuristic, [OpenedFacts|ForbiddenStates], Plan, PlanCost, FinalFacts).
-
-openNonForbiddenActionsInHeap(Namespace, ForbiddenStates, Heuristic, Heap, NewHeap, OpenedFacts) :-
-  %write("BEGIN OPENING ---------------------------- "),
-  get_from_heap(Heap, HalfPlanCost, halfPlanExecution(HalfPlan, OpenedFacts), PopedHeap),
-  %reverse(HalfPlan, WritePlan), write(WritePlan), write(" --------- "), write(ForbiddenStates), nl,
-  findall(actionExecution(Action, ObtainedFacts), allowedActionExecution(Namespace, Action, OpenedFacts, ObtainedFacts), AllActionExecutions),
-  filterNonForbiddenActionExecutions(AllActionExecutions, ForbiddenStates, NonForbiddenActions),
-  %write("NONFORB --------- "), write(NonForbiddenActions), nl,
-  addPlanExecutionsToHeap(NonForbiddenActions, Heuristic, HalfPlanCost, HalfPlan, PopedHeap, NewHeap).
-
-filterNonForbiddenActionExecutions([], _, []).
-filterNonForbiddenActionExecutions([actionExecution(_, ObtainedFacts)|RestOfActionExecutions], ForbiddenStates, NonForbiddenActionExecutions) :-
-  setIsOneOf(ObtainedFacts, ForbiddenStates), !, % red cut
-  filterNonForbiddenActionExecutions(RestOfActionExecutions, ForbiddenStates, NonForbiddenActionExecutions).
-filterNonForbiddenActionExecutions([actionExecution(Action, ObtainedFacts)|RestOfActionExecutions], ForbiddenStates, [actionExecution(Action, ObtainedFacts)|RestOfNonForbiddenActionExecutions]) :-
-  filterNonForbiddenActionExecutions(RestOfActionExecutions, ForbiddenStates, RestOfNonForbiddenActionExecutions).
-
-addPlanExecutionsToHeap([], _, _, _, Heap, Heap).
-addPlanExecutionsToHeap([actionExecution(Action, ObtainedFacts)|RestOfActionExecutions], Heuristic, HalfPlanCost, HalfPlan, Heap, NewHeap) :-
-  call(Heuristic, actionExecution(Action, ObtainedFacts), Cost),
-  TotalCost is HalfPlanCost + Cost,
-  addHalfPlanToHeapIfBetterOrEqual(Heap, TotalCost, halfPlanExecution([Action|HalfPlan], ObtainedFacts), MiddleHeap),
-  %write("Cost: "), write(TotalCost), write(", Action: "), write(Action), nl,
-  addPlanExecutionsToHeap(RestOfActionExecutions, Heuristic, HalfPlanCost, HalfPlan, MiddleHeap, NewHeap).
-
-addHalfPlanToHeapIfBetterOrEqual(Heap, HalfPlanCost, HalfPlanExecution, NewHeap) :-
-  empty_heap(Heap), !, % red cut
-  add_to_heap(Heap, HalfPlanCost, HalfPlanExecution, NewHeap).
-addHalfPlanToHeapIfBetterOrEqual(Heap, HalfPlanCost, halfPlanExecution(HalfPlan, Facts), NewHeap) :-
-  get_from_heap(Heap, PopedCost, halfPlanExecution(PopedHalfPlan, PopedFacts), PopedHeap),
-  (
-    equivalentTo(PopedFacts, Facts), !, % red cut
-    (
-      HalfPlanCost =:= PopedCost, !, % green cut
-      add_to_heap(PopedHeap, PopedCost, halfPlanExecution(PopedHalfPlan, PopedFacts), MiddleHeap),
-      add_to_heap(MiddleHeap, HalfPlanCost, halfPlanExecution(HalfPlan, Facts), NewHeap)
-    ;
-      HalfPlanCost < PopedCost, !, % green cut
-      add_to_heap(PopedHeap, HalfPlanCost, halfPlanExecution(HalfPlan, Facts), NewHeap)
-    ;
-      HalfPlanCost > PopedCost,
-      add_to_heap(PopedHeap, PopedCost, halfPlanExecution(PopedHalfPlan, PopedFacts), NewHeap)
-    )
-  ;
-    % \+ equivalentTo(PopedFacts, Facts)
-    addHalfPlanToHeapIfBetterOrEqual(PopedHeap, HalfPlanCost, halfPlanExecution(HalfPlan, Facts), AddedHeap),
-    add_to_heap(AddedHeap, PopedCost, halfPlanExecution(PopedHalfPlan, PopedFacts), NewHeap)
-  ).
 
 % -------------------- Private predicates
 
@@ -473,7 +470,7 @@ updatedFacts(Facts, RemovedFacts, AddedFacts, NewFacts) :-
 % @arg Facts A set of facts.
 % @arg NewFacts A set facts obtained after applying Action in Facts.
 %
-% True if Action can be applied in the set of Facts (conditions are satisfied) and
+%   True if Action can be applied in the set of Facts (conditions are satisfied) and
 % NewFacts is the resulting set after applying the Action. NewFacts must have a
 % specific order.
 allowedActionExecution(Namespace, Action, Facts, NewFacts) :-
@@ -487,6 +484,10 @@ allowedActionExecution(Namespace, Action, Facts, NewFacts) :-
   updatedFacts(Facts, RemovedFacts, AddedFacts, NewFacts),
   executeSideEffects(PrologSideEffects).
 
+% satisfiedPlannerGoals(++Facts:list, ++Goals:list) is semidet
+%
+%   True if planner Goals are satisfied by the Facts. Goals like "not(x)" or "\+ x" are satisfied if the fact
+% doesn't exist in Facts.
 satisfiedPlannerGoals(Facts, Goals) :-
   separatedGoals(Goals, InclusionGoals, AbsenceGoals),
   subsetOf(InclusionGoals, Facts),
@@ -505,7 +506,7 @@ satisfiedPlannerGoals(Facts, Goals) :-
 %                      when the plan is followed starting from Facts.
 % @arg FinalFacts The set of facts that remains after Plan is executed.
 %
-% True if all Goals are satisfied after the sequence of actions in Plan is applied
+%   True if all Goals are satisfied after the sequence of actions in Plan is applied
 % to the Facts, without passing thorugh any state in ForbiddenStates. 
 planWithForbiddenStates(_, Facts, Goals, [], _, Facts) :-
   satisfiedPlannerGoals(Facts, Goals), !. % red cut
@@ -513,3 +514,104 @@ planWithForbiddenStates(Namespace, Facts, Goals, [FirstAction|RestOfPlan], Forbi
   allowedActionExecution(Namespace, FirstAction, Facts, NewFacts),
   not(setIsOneOf(NewFacts, ForbiddenStates)),
   planWithForbiddenStates(Namespace, NewFacts, Goals, RestOfPlan, [NewFacts|ForbiddenStates], FinalFacts).
+
+% planAStar_(++Namespace:atom, ++Heap:heap, ++Goals:list, :Heuristic:atom, ++ForbiddenStates:list, -Plan:list, -PlanCost:number, -FinalFacts:list) is nondet
+% 
+% @arg Namespace This could be any atom. The Namespace used here should match the one used in "endDomainDefinition" and "endProblemDefinition".
+%
+%   True if Plan is a list of actions that make the planer Goals be satisfied from the initial facts. The halfPlanExecutions being computed are stored
+% in the Heap. The initial facts should already be consistent with the information on the Heap. The Plan to be computed won't pass through any set of facts
+% equivalent to those in ForbiddenStates. The Heuristic meta predicate determines the order in which halfPlanExecutions are computed (through heap priority)
+% by associating costs to each action. That also determines the total PlanCost, which is returned. FinalFacts is the set of facts that remain after the plan
+% is executed.
+planAStar_(Namespace, Heap, Goals, Heuristic, ForbiddenStates, Plan, PlanCost, FinalFacts) :-
+  min_of_heap(Heap, HalfPlanCost, halfPlanExecution(HalfPlan, Facts)),
+  satisfiedPlannerGoals(Facts, Goals),
+  (
+    Plan = HalfPlan,
+    PlanCost = HalfPlanCost,
+    FinalFacts = Facts
+  ;
+    !, % red cut
+    get_from_heap(Heap, _, _, PopedHeap),
+    planAStar_(Namespace, PopedHeap, Goals, Heuristic, ForbiddenStates, Plan, PlanCost, FinalFacts)
+  ).
+planAStar_(Namespace, Heap, Goals, Heuristic, ForbiddenStates, Plan, PlanCost, FinalFacts) :-
+  openNonForbiddenActionsInHeap(Namespace, ForbiddenStates, Heuristic, Heap, NewHeap, OpenedFacts),
+  \+ empty_heap(NewHeap),
+  planAStar_(Namespace, NewHeap, Goals, Heuristic, [OpenedFacts|ForbiddenStates], Plan, PlanCost, FinalFacts).
+
+% openNonForbiddenActionsInHeap(++Namespace:atom, ++ForbiddenStates:list, :Heuristic:atom, ++Heap:heap, -NewHeap:heap, -OpenedFacts:list) is det
+% 
+% @arg Namespace This could be any atom. The Namespace used here should match the one used in "endDomainDefinition" and "endProblemDefinition".
+% @arg ForbiddenStates List of fact sets that were already opened in a previous heap opening. Plans that achieve the same set of facts shouldn't
+%                      be added to heap again.
+% @arg Heuristic Meta predicate that receives an actionExecution and returns its cost, so it can be added to the heap's planExecutions.
+%
+%   This assumes the Heap is not empty. Heap contains halfPlanExecution(HalfPlan, OpenedFacts). This predicate removes
+% the highest priority entry (lowest value), computates all valid next actions and adds them to the Heap as new
+% halfPlanExecutions. It returns the new heap in NewHeap and the facts that resulted from the opened halfPlanExecution
+% in OpenedFacts.
+openNonForbiddenActionsInHeap(Namespace, ForbiddenStates, Heuristic, Heap, NewHeap, OpenedFacts) :-
+  get_from_heap(Heap, HalfPlanCost, halfPlanExecution(HalfPlan, OpenedFacts), PopedHeap),
+  findall(actionExecution(Action, ObtainedFacts), allowedActionExecution(Namespace, Action, OpenedFacts, ObtainedFacts), AllActionExecutions),
+  filterNonForbiddenActionExecutions(AllActionExecutions, ForbiddenStates, NonForbiddenActions),
+  addPlanExecutionsToHeap(NonForbiddenActions, Heuristic, HalfPlanCost, HalfPlan, PopedHeap, NewHeap).
+
+% filterNonForbiddenActionExecutions(++ActionExecutions:list, ++ForbiddenStates:list, -NonForbiddenActionExecutions:list) is det
+% 
+%   Receives a ActionExecutions list of actionExecution(Action:compound_term, ObtainedFacts:list). This checks whether
+% each ObtainedFacts set has an equivalent set in ForbiddenStates. If has, that means that action can't be executed,
+% so it is filtered out of that list. The new filtered list is returned in NonForbiddenActionExecutions.
+filterNonForbiddenActionExecutions([], _, []).
+filterNonForbiddenActionExecutions([actionExecution(_, ObtainedFacts)|RestOfActionExecutions], ForbiddenStates, NonForbiddenActionExecutions) :-
+  setIsOneOf(ObtainedFacts, ForbiddenStates), !, % red cut
+  filterNonForbiddenActionExecutions(RestOfActionExecutions, ForbiddenStates, NonForbiddenActionExecutions).
+filterNonForbiddenActionExecutions([actionExecution(Action, ObtainedFacts)|RestOfActionExecutions], ForbiddenStates, [actionExecution(Action, ObtainedFacts)|RestOfNonForbiddenActionExecutions]) :-
+  filterNonForbiddenActionExecutions(RestOfActionExecutions, ForbiddenStates, RestOfNonForbiddenActionExecutions).
+
+% addPlanExecutionsToHeap(++ActionExecutions:list, :Heuristic:atom, ++HalfPlanCost:number, ++HalfPlan:list, ++Heap:heap, NewHeap:heap) is det
+% 
+%   Receives a Heap of halfPlanExecution(HalfPlan:list, ObtainedFacts:list) and an ActionExecutions list of
+% actionExecution(Action:compound_term, ObtainedFacts:list). The meta predicate Heuristic is called for each actionExecution, retrieving
+% the costOfAction of that action. Adds new halfPlanExecutions to the heap for each actionExecution, with priority equal to TotalCost =
+% HalfPlanCost + costOfAction. Each new halfPlan is the HalfPlan with the Action as the head of the list (the plans are reversed). Note
+% that the heap addition only happens if there is no entry in the heap which already contains an equivalent ObtainedFacts set and better
+% cost (lower priority).
+addPlanExecutionsToHeap([], _, _, _, Heap, Heap).
+addPlanExecutionsToHeap([actionExecution(Action, ObtainedFacts)|RestOfActionExecutions], Heuristic, HalfPlanCost, HalfPlan, Heap, NewHeap) :-
+  call(Heuristic, actionExecution(Action, ObtainedFacts), Cost),
+  TotalCost is HalfPlanCost + Cost,
+  addHalfPlanToHeapIfBetterOrEqual(Heap, TotalCost, halfPlanExecution([Action|HalfPlan], ObtainedFacts), MiddleHeap),
+  addPlanExecutionsToHeap(RestOfActionExecutions, Heuristic, HalfPlanCost, HalfPlan, MiddleHeap, NewHeap).
+
+% addHalfPlanToHeapIfBetterOrEqual(++Heap:heap, ++HalfPlanCost:number, ++HalfPlanExecution:compound_term, -NewHeap:heap) is det
+% 
+%   Receives a Heap of halfPlanExecution(HalfPlan:list, ObtainedFacts:list) and tries to add HalfPlanExecution
+% of priority HalfPlanCost. It searches the heap for an equivalent ObtainedFacts set. If there isn't one, then
+% HalfPlanExecution is added. If there is one, then it looks at its cost and replaces that entry for
+% HalfPlanExecution if HalfPlanCost is better costed (less than the one in the Heap). If the costs are equal,
+% HalfPlanExecution is simply added and nothing is replaced or removed. The new heap is returned in NewHeap.
+addHalfPlanToHeapIfBetterOrEqual(Heap, HalfPlanCost, HalfPlanExecution, NewHeap) :-
+  empty_heap(Heap), !, % red cut
+  add_to_heap(Heap, HalfPlanCost, HalfPlanExecution, NewHeap).
+addHalfPlanToHeapIfBetterOrEqual(Heap, HalfPlanCost, halfPlanExecution(HalfPlan, Facts), NewHeap) :-
+  get_from_heap(Heap, PopedCost, halfPlanExecution(PopedHalfPlan, PopedFacts), PopedHeap),
+  (
+    equivalentTo(PopedFacts, Facts), !, % red cut
+    (
+      HalfPlanCost =:= PopedCost, !, % green cut
+      add_to_heap(PopedHeap, PopedCost, halfPlanExecution(PopedHalfPlan, PopedFacts), MiddleHeap),
+      add_to_heap(MiddleHeap, HalfPlanCost, halfPlanExecution(HalfPlan, Facts), NewHeap)
+    ;
+      HalfPlanCost < PopedCost, !, % green cut
+      add_to_heap(PopedHeap, HalfPlanCost, halfPlanExecution(HalfPlan, Facts), NewHeap)
+    ;
+      HalfPlanCost > PopedCost,
+      add_to_heap(PopedHeap, PopedCost, halfPlanExecution(PopedHalfPlan, PopedFacts), NewHeap)
+    )
+  ;
+    % \+ equivalentTo(PopedFacts, Facts)
+    addHalfPlanToHeapIfBetterOrEqual(PopedHeap, HalfPlanCost, halfPlanExecution(HalfPlan, Facts), AddedHeap),
+    add_to_heap(AddedHeap, PopedCost, halfPlanExecution(PopedHalfPlan, PopedFacts), NewHeap)
+  ).
