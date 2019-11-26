d49f30525b98aeab8154f406015d57966cbd44ce
Rafael Rubim <rafael.rubim.cabral@gmail.com>
2019-11-26 04:27:00 -0300

    Rearrange medievalStory.pl

--- story-generator/src/prolog/medievalStory.pl
+++ story-generator/src/prolog/medievalStory.pl
@@ -1,33 +1,25 @@
-:- use_module(entity).
-:- use_module(enumeration).
-:- use_module(state).
-:- use_module(event).
-:- use_module(trigger).
-:- use_module(eventProcesser).
+:- use_module(index).
+
+:- beginEnumsDefinition.
+:- beginEntityTypesDefinition.
+:- beginStateTypesDefinition.
+:- beginTriggerTypesDefinition.
+:- beginEventTypesDefinition.
 
 % >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Genre Definition
 
 % --------------------------------- Enum types
-:- beginEnumsDefinition.
 
 enumSpec(speed, [slow, medium, fast]).
 
-:- endEnumsDefinition.
-
 % --------------------------------- Entity types
-:- beginEntityTypesDefinition.
-
 categorySpec(man, character).
 categorySpec(woman, character).
 categorySpec(prince, man).
 categorySpec(horse, animal).
 categorySpec(building, place).
 
-:- endEntityTypesDefinition.
-
 % --------------------------------- State types
-:- beginStateTypesDefinition.
-
 signatureSpec(builtIn, [
   entityArg(building),
   entityArg(place)
@@ -48,20 +40,11 @@ signatureSpec(savedBy, [
   entityArg(character)
 ]).
 
-:- endStateTypesDefinition.
-
 % --------------------------------- Trigger types
-:- beginTriggerTypesDefinition.
-
 triggerTypeSpec(villainStrikes, active).
 triggerTypeSpec(heroActs, active).
 
-:- endTriggerTypesDefinition.
-
 % --------------------------------- Event types
-
-:- beginEventTypesDefinition.
-
 eventTypeSpec(
   kidnap(Kidnapper, Kidnapped, Plc),
   [standsIn(Kidnapper, Plc), standsIn(Kidnapped, Plc)],
@@ -72,42 +55,37 @@ eventTypeSpec(
   [kidnappedBy(Kidnapped, Kidnapper)]
 ).
 
+:- endEnumsDefinition.
+:- endEntityTypesDefinition.
+:- endStateTypesDefinition.
+:- endTriggerTypesDefinition.
 :- endEventTypesDefinition.
 
 % >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Story definition
 
-% --------------------------------- Entities
 :- beginEntitiesDefinition.
+:- beginStatesDefinition.
+:- beginTriggersDefinition.
+:- beginPlotDefinition.
 
+% --------------------------------- Entities
 entitySpec(cassandra, woman).
 entitySpec(horace, prince).
 entitySpec(capital, place).
 entitySpec(palace, building).
 entitySpec(morgarath, man).
 
-:- endEntitiesDefinition.
-
 % --------------------------------- States
-:- beginStatesDefinition.
-
 builtIn(palace, capital).
 isKnight(cassandra).
 standsIn(horace, palace).
 standsIn(cassandra, palace).
 standsIn(morgarath, capital).
 
-:- endStatesDefinition.
-
 % --------------------------------- Triggers
-:- beginTriggersDefinition.
-
 villainStrikes(10).
 
-:- endTriggersDefinition.
-
 % --------------------------------- Plot
-:- beginPlotDefinition.
-
 plotSpec([
   [
     kidnappedBy(horace, Villain)
@@ -122,8 +100,9 @@ plotSpec([
   ]
 ]).
 
+:- endEntitiesDefinition.
+:- endStatesDefinition.
+:- endTriggersDefinition.
 :- endPlotDefinition.
 
 :- beginEventProcesser.
-
-% planToPlot(Plan).
